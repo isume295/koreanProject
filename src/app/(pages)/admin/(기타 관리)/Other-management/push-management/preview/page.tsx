42cc5fb4d871bfe848c86bb5d2997175db29f695
nanati45 <nansiasamnew@gmail.com>
2025-01-01 20:15:04 +0300

    modal ui added

--- src/app/(pages)/admin/(기타 관리)/Other-management/push-management/preview/page.tsx
+++ src/app/(pages)/admin/(기타 관리)/Other-management/push-management/preview/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Preview from "../components/Preview";
+import WaitForShipmentModal from "../components/WaitForShipmentModal";
 
 const PreviewDetails = () => {
   return (
     <div>
       <Preview />
+      <WaitForShipmentModal />
     </div>
   );
 };

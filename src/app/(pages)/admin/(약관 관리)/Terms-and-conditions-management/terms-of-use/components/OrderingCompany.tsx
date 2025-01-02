d36a57b80348cefdde051aaa5cc1ee2dd353a0a5
MubarakAd <mubaarakadem@gmail.com>
2025-01-02 12:06:30 +0300

    modify table button

--- src/app/(pages)/admin/(약관 관리)/Terms-and-conditions-management/terms-of-use/components/OrderingCompany.tsx
+++ src/app/(pages)/admin/(약관 관리)/Terms-and-conditions-management/terms-of-use/components/OrderingCompany.tsx
@@ -5,7 +5,10 @@ import React, { useState } from "react";
 import ModalWrapper from "@/src/components/blocks/Modals/ModalWrapper";
 import MarkDown from "@/src/components/blocks/markdown/MarkDown";
 import InputNoLabel from "@/src/components/blocks/inputs/datePickerInput";
-import { EquipmentOperatorHeader, EquipmentOperatorTableData, } from "./TermUseTableData";
+import {
+  EquipmentOperatorHeader,
+  EquipmentOperatorTableData,
+} from "./TermUseTableData";
 
 const OrderingCompany = () => {
   const [showRegistration, setShowRegistration] = useState(false);
@@ -14,22 +17,15 @@ const OrderingCompany = () => {
   };
   return (
     <div>
-      <div className="flex justify-end">
-        <Button
-          label="등록"
-          backgroundColor="bg-[#404251]"
-          textStyle="text-sm text-white"
-          padding="px-[20px] py-[16px]"
-          borderRadius="rounded-[100px]"
-          width="w-[66px]"
-          onPress={handleRegistration}
-        />
-      </div>
-      <div className="mt-[14px]">
+      <div className="mt-[10px]">
         <Table
           data={EquipmentOperatorTableData}
           header={EquipmentOperatorHeader}
           hasPagination={true}
+          buttonLabel="등록"
+          buttonRadius="rounded-[100px]"
+          buttonWidth="w-[66px]"
+          onClick={handleRegistration}
         />
       </div>
       {showRegistration && (
@@ -44,7 +40,7 @@ const OrderingCompany = () => {
             <div className="flex gap-5">
               <p className="min-w-[81px] text-sm text-[#333333]">내용</p>
               <div>
-                <MarkDown  />
+                <MarkDown />
               </div>
             </div>
             <div className="flex justify-center items-center gap-[16px] mt-[40px]">

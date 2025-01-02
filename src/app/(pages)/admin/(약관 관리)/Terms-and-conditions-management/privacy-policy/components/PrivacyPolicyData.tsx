9de54d5fdd74bc55bc89cab5ba0e191f9325fc55
MubarakAd <mubaarakadem@gmail.com>
2025-01-02 10:08:08 +0300

    create privacy policy page

---
+++ src/app/(pages)/admin/(약관 관리)/Terms-and-conditions-management/privacy-policy/components/PrivacyPolicyData.tsx
@@ -0,0 +1,135 @@
+import Link from "next/link";
+
+export const PrivacyPolicyHeader = [
+  { label: "번호" },
+  { label: "제목" },
+  { label: "관리자" },
+  { label: "처리일자" },
+];
+export const PrivacyPolicaTableData = [
+{   number: 1 ,
+  
+    title: (
+      <Link href={""} className="text-[#429FFF] underline">
+        개인정보 처리방침 v0.5
+      </Link>
+    ),
+  
+  manager: "이중재" ,
+   processdate: "2023-08-30 10:11:28" },
+   {   number: 1 ,
+  
+    title: (
+      <Link href={""} className="text-[#429FFF] underline">
+        개인정보 처리방침 v0.5
+      </Link>
+    ),
+  
+  manager: "이중재" ,
+   processdate: "2023-08-30 10:11:28" },
+   {   number: 1 ,
+  
+    title: (
+      <Link href={""} className="text-[#429FFF] underline">
+        개인정보 처리방침 v0.5
+      </Link>
+    ),
+  
+  manager: "이중재" ,
+   processdate: "2023-08-30 10:11:28" },
+   {   number: 1 ,
+  
+    title: (
+      <Link href={""} className="text-[#429FFF] underline">
+        개인정보 처리방침 v0.5
+      </Link>
+    ),
+  
+  manager: "이중재" ,
+   processdate: "2023-08-30 10:11:28" },{   number: 1 ,
+  
+    title: (
+      <Link href={""} className="text-[#429FFF] underline">
+        개인정보 처리방침 v0.5
+      </Link>
+    ),
+  
+  manager: "이중재" ,
+   processdate: "2023-08-30 10:11:28" },{   number: 1 ,
+  
+    title: (
+      <Link href={""} className="text-[#429FFF] underline">
+        개인정보 처리방침 v0.5
+      </Link>
+    ),
+  
+  manager: "이중재" ,
+   processdate: "2023-08-30 10:11:28" }
+];
+export const EquipmentBusinessHeader = [
+    { label: "번호" },
+    { label: "제목" },
+    { label: "관리자" },
+    { label: "처리일자" },
+  ];
+  export const EquipmentBusinessTableData = [
+  {   number: 1 ,
+    
+      title: (
+        <Link href={""} className="text-[#429FFF] underline">
+          개인정보 처리방침 v0.5
+        </Link>
+      ),
+    
+    manager: "이중재" ,
+     processdate: "2023-08-30 10:11:28" },
+     {   number: 1 ,
+    
+      title: (
+        <Link href={""} className="text-[#429FFF] underline">
+          개인정보 처리방침 v0.5
+        </Link>
+      ),
+    
+    manager: "이중재" ,
+     processdate: "2023-08-30 10:11:28" },
+     {   number: 1 ,
+    
+      title: (
+        <Link href={""} className="text-[#429FFF] underline">
+          개인정보 처리방침 v0.5
+        </Link>
+      ),
+    
+    manager: "이중재" ,
+     processdate: "2023-08-30 10:11:28" },
+     {   number: 1 ,
+    
+      title: (
+        <Link href={""} className="text-[#429FFF] underline">
+          개인정보 처리방침 v0.5
+        </Link>
+      ),
+    
+    manager: "이중재" ,
+     processdate: "2023-08-30 10:11:28" },{   number: 1 ,
+    
+      title: (
+        <Link href={""} className="text-[#429FFF] underline">
+          개인정보 처리방침 v0.5
+        </Link>
+      ),
+    
+    manager: "이중재" ,
+     processdate: "2023-08-30 10:11:28" },{   number: 1 ,
+    
+      title: (
+        <Link href={""} className="text-[#429FFF] underline">
+          개인정보 처리방침 v0.5
+        </Link>
+      ),
+    
+    manager: "이중재" ,
+     processdate: "2023-08-30 10:11:28" }
+  ];
+  
--- src/app/(pages)/admin/(약관 관리)/Terms-and-conditions-management/privacy-policy/page.tsx
+++ src/app/(pages)/admin/(약관 관리)/Terms-and-conditions-management/privacy-policy/page.tsx
@@ -1,7 +1,25 @@
+'use client'
+import CustomTab from "@/src/components/blocks/tabs/Tabs";
 import React from "react";
+import PrivacyPolicy from "./components/PrivacyPolicy";
+import UsageManagement from "../../../(결제 관리)/Payment-management/cache-management/components/usageManagement";
+import EquipmentBusiness from "./components/EquipmentBusiness";
 
-const PrivacyPolicy = () => {
-  return <div>PrivacyPolicy</div>;
-};
+const Page = () => {
+  const Tabs = [
+    { title: "발주사", content: <PrivacyPolicy/> },
+    { title: "장비사업자", content: <EquipmentBusiness/> },
+  ];
+  return (
+    <div className="">
+      <CustomTab
+        items={Tabs}
+        containerPadding="p-[6px]"
+        raduis="rounded-[14px]"
+        selectedBgColor="bg-[#404251]"
+      />
+    </div>
+  );
+}
 
-export default PrivacyPolicy;
+export default Page;

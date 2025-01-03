9d18ab44e3866b6fa289691f7ade1f0d3c41e152
sumeya <sumeya@addissoftware.com>
2025-01-03 22:14:01 +0300

    fix: disable footer in ChatModal component

--- src/components/blocks/chat/chatModal.tsx
+++ src/components/blocks/chat/chatModal.tsx
@@ -20,6 +20,7 @@ const ChatModal = ({
       closeButton={true}
       modalWidthInPercent="max-w-[50%]"
       paddingHorizontal="px-0 py-0"
+      footer={false}
     >
       <div className=" w-full flex justify-center items-center pt-[40px] pb-[0px]">
         <ChatScreen senderName={senderName} profileName={profileName} />
